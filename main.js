b5d38fc698f85c487dbf4ac650c27e8be4725b98
helenaKuthanova <helena.kuthanova@gmail.com>
2022-02-23 14:42:03 +0100

    upravy podle Matejova reseni

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 // hází náhodná čísla od 1 do 6
 
 function hodKostkou() {
-    let hod = Math.ceil(Math.random() * 6)
-    let zprava = document.querySelector("#zprava")
-    let obrazek = document.getElementById("kostka")
+    let zprava = document.querySelector("#zprava");
+    let obrazek = document.getElementById("kostka");
+    // zpravu i obrazek do lokálního scope - můžu pak celou fci importovat do jiného programu
 
-    if (hod < 6) {
-        zprava.textContent = "Hodila jsi " + hod + ". Házej znovu."
-        obrazek.src = "obrazky/" + hod + ".png"
-        console.log(hod)
+    let hod = Math.floor(Math.random() * 6 + 1);
+    // hod potřebuji generovat při každém volání funkce = kliku
+    // použiju Math.floor pro případ, že by padla nula (což může)
+
+    if (hod === 6) {
+        zprava.textContent = "Šestka! Vyhrála jsi.";
+        console.log(hod);
     }
     else {
-        zprava.textContent = "Šestka! Vyhrála jsi."
-        obrazek.src = "obrazky/6.png"
+        zprava.textContent = "Hodila jsi " + hod + ". Házej znovu.";
+        console.log(hod);
     }
+
+    obrazek.src = "obrazky/" + hod + ".png";
 }
+    // můžu dát mimo IF - bude se dít u všech
