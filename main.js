911202d9989b79d22e56c51859c5d676dbec8ed9
helenaKuthanova <helena.kuthanova@gmail.com>
2022-02-23 13:32:41 +0100

    vypisuje zprávu s hodnotou hodu

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-// tady je místo pro náš program
+// hází náhodná čísla od 1 do 6
+
+function hodKostkou() {
+    let hod = Math.ceil(Math.random() * 6)
+    let zprava = document.querySelector("#zprava")
+
+    if (hod < 6) {
+        zprava.textContent = "Hodila jsi " + hod + ". Házej znovu."
+        console.log(hod)
+    }
+    else {
+        zprava.textContent = "Šestka! Vyhrála jsi."
+    }
+}
