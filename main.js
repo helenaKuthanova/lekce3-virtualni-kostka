5244bb2b7098c984a36c312219715016aa5b65e2
helenaKuthanova <helena.kuthanova@gmail.com>
2022-02-23 13:43:41 +0100

    doplněna výměna obrázku s kostkou

--- main.js
+++ main.js
@@ -3,12 +3,15 @@
 function hodKostkou() {
     let hod = Math.ceil(Math.random() * 6)
     let zprava = document.querySelector("#zprava")
+    let obrazek = document.getElementById("kostka")
 
     if (hod < 6) {
         zprava.textContent = "Hodila jsi " + hod + ". Házej znovu."
+        obrazek.src = "obrazky/" + hod + ".png"
         console.log(hod)
     }
     else {
         zprava.textContent = "Šestka! Vyhrála jsi."
+        obrazek.src = "obrazky/6.png"
     }
 }
